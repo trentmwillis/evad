a70b5e9cfdd3f50d41c15abe108342ffedfd661e
Kim Chan <kim.chan@ufl.edu>
2013-04-13 21:36:48 -0400

    Mayor Bongo play fix

--- Assets/Standard Assets/Scripts/MayorPlayScript.js
+++ Assets/Standard Assets/Scripts/MayorPlayScript.js
@@ -1,6 +1,8 @@
 #pragma strict
 
 var mayorPlay : boolean;
+var incorrect : boolean;
+var win : boolean;
 var bongoSequence : int[];
 var marcBongoSequence : int[];
 var mayorCount : int;
@@ -16,28 +18,71 @@ var bongo5 : AudioClip;
 
 var mayor: GameObject;
 var mayorAnimations: Array;
+var mayorNumber = -1;
+var style : GUIStyle;
 
 
 function Start () 
 {
 	beatCount = 8;
-	bongoSequence = [1,2,5,4,5,2,2,3];
+	bongoSequence = [1,2,5,4,5,2,3,2];
 	marcBongoSequence = new int[beatCount];
 	mayorCount = 1;
 	marcCount = 0;
 	correct = true;
+	mayorPlay = true;
+	win = false;
 	mayorAnimations = new Array();
 	mayor = GameObject.Find("Mayor_bongo");
-	mayorAnimation();
+	style = new GUIStyle();
+	style.fontSize = 20;
+	style.normal.textColor = Color.white;
+	incorrect = false;
+}
+
+function OnGUI () {
+	GUI.contentColor = Color.white;
+	if(mayorPlay)
+	{
+    	GUI.Box (Rect (Screen.width/15, Screen.height/2, 500,30), "Use the number keys to follow the Mayor's beat. Click on the screen to start.", style);
+    }
+    if(mayorNumber != -1)
+    {
+    	style.fontSize = 16;
+    	GUI.Label (Rect (Screen.width/2, 2*Screen.height/3+25, 100, 20), "" + mayorNumber);
+    }
+    if(incorrect)
+    {
+    	style.fontSize = 20;
+    	GUI.Box (Rect (Screen.width/2-60, Screen.height/2, 500,30), "You didn't follow me very well. \nClick to try again", style);
+    }
+    if(win)
+    {
+    	style.fontSize = 20;
+    	GUI.Box (Rect (Screen.width/2-60, Screen.height/2, 500,30), "Thanks for your help! \nTake a tank of gas for your troubles.", style);
+    }
 }
 
 function Update () 
 {
-	if(mayorCount == beatCount)
+	if(Input.GetMouseButtonDown(0) && mayorPlay)
+	{
+		mayorPlay = false;
+		mayorAnimation();
+	}
+	if(Input.GetMouseButtonDown(0) && incorrect )
+	{
+		incorrect = false;
+		mayorAnimation();
+	}
+	if(mayorCount-1 == beatCount)
 	{
 		//game over
 		marcCount=0;
+		win = true;
 	}
+	if(!win)
+	{
 		if(Input.GetKeyDown("1"))
 		{
 			marcBongoSequence[marcCount] = 1;
@@ -68,7 +113,12 @@ function Update ()
 			marcCount++;
 			checkFinished();
 		}	
-	
+	}
+	else
+	{
+    	//Load Next level
+    	Debug.Log("done");
+	}
 }
 
 function checkFinished()
@@ -76,6 +126,7 @@ function checkFinished()
 	if(marcCount == mayorCount)
 	{
 		checkSequence();
+		marcBongoSequence = new int[beatCount];
 	}
 }
 
@@ -91,54 +142,66 @@ function checkSequence()
 	
 	if(correct)
 	{
-		mayorCount ++;
 		marcCount = 0;
 		//Show Mayor Sequence
-		mayorAnimation();
+		if(!win)
+		{
+			mayorCount ++;
+			mayorAnimation();
+		}
 	}
 	else
 	{
 		mayorCount = 1;
 		marcCount = 0;
-		mayorAnimation();
+		incorrect = true;
 	}
 	correct = true;
 }
 
 function mayorAnimation()
 {
+if(!win)
+{
 	yield WaitForSeconds(1);
 	for(var i = 0; i < mayorCount; i++)
 	{
 		if(bongoSequence[i] == 1)
 		{
+			mayorNumber = 1;
 			mayor.animation.Play("Bongo1");
 			yield WaitForSeconds(1);
 			audio.PlayOneShot(bongo1);
 		}
 		else if(bongoSequence[i] == 2)
 		{
+			mayorNumber = 2;
 			mayor.animation.Play("Bongo2");
 			yield WaitForSeconds(1);
 			audio.PlayOneShot(bongo2);
 		}
 		else if(bongoSequence[i] == 3)
 		{
+			mayorNumber = 3;
 			mayor.animation.Play("Bongo3");
 			yield WaitForSeconds(1);
 			audio.PlayOneShot(bongo3);
 		}
 		else if(bongoSequence[i] == 4)
 		{
+			mayorNumber = 4;
 			mayor.animation.Play("Bongo4");
 			yield WaitForSeconds(1);
 			audio.PlayOneShot(bongo4);
 		}
 		else if(bongoSequence[i] == 5)
 		{
+			mayorNumber = 5;
 			mayor.animation.Play("Bongo5");
 			yield WaitForSeconds(1);
 			audio.PlayOneShot(bongo5);
 		}
+		mayorNumber = -1;
+	}
 	}
 }
